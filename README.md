2fcff7549b8f58ffb92e3307a45afa161f450157
VarHug <490641570@qq.com>
2021-02-04 11:04:30 +0800

    feat: 🎸 add week-01 groupAnagrams

--- README.md
+++ README.md
@@ -6,8 +6,16 @@
 * adjust: 将数组中指定索引处的值替换为经函数变换的值
   * 实现版本：[https://github.com/VarHug/quiz/blob/master/src/week-01/index.ts](https://github.com/VarHug/quiz/blob/master/src/week-01/index.ts)
   * ramda版本参考：[https://github.com/ramda/ramda/blob/master/source/adjust.js](https://github.com/ramda/ramda/blob/master/source/adjust.js)
+* groupAnagrams: 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+  * 题目参考：[https://leetcode.com/problems/group-anagrams/](https://leetcode.com/problems/group-anagrams/)
+  * 实现版本：[]()
+  * 题解参考：[https://leetcode.com/problems/group-anagrams/discuss/?currentPage=1&orderBy=hot&query=](https://leetcode.com/problems/group-anagrams/discuss/?currentPage=1&orderBy=hot&query=)
 ## 题目归档
 
 ### List（数组）
 * adjust
 
+### 算法
+
+#### 哈希表
+* groupAnagrams
--- src/week-01/index.ts
+++ src/week-01/index.ts
@@ -25,3 +25,30 @@ export const adjust: IAdjustFunc = (idx: number, fn: Function, list: any[]): any
   _list[_idx] = fn(_list[_idx]);
   return _list;
 };
+
+/**
+ * @description 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ * @sig Array -> Array
+ * @param {Array} strs
+ * @returns {Array}
+ * @example
+ *      groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]);      //=> [["ate","eat","tea"],["nat","tan"],["bat"]]
+ * @score 50
+ */
+export const groupAnagrams: (strs: string[]) => string[][] = (strs: string[]): string[][] => {
+  const dic: {
+    [propName: string]: number
+  } = {};
+  const ret: string[][] = [];
+  let counter: number = 0;
+  strs.forEach((str: string): void => {
+    const key: string = str.split('').sort().join('');
+    if (typeof dic[key] !== 'number') {
+      dic[key] = counter;
+      ret[counter] = [];
+      counter++;
+    }
+    ret[dic[key]].push(str);
+  });
+  return ret;
+};
