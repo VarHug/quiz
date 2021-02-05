de0005c22ce23aa91a853b090361ced65ff8f597
VarHug <490641570@qq.com>
2021-02-05 14:51:02 +0800

    feat: 🎸 update week-03 question

--- src/week-03/__tests__/index.test.ts
+++ src/week-03/__tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { less, swap } from '../index';
+import { less, swap, sortColors } from '../index';
 
 test('less', () => {
   expect(less(1, 10)).toBe(-1);
@@ -13,4 +13,16 @@ test('swap', () => {
   swap(list, 1, 2, true);
   expect(list).toEqual([1, 3, 2, 4]);
   expect(swap([1, 2, 3, 4], 0, 1, false)).toEqual([2, 1, 3, 4]);
+});
+
+test('sortColors', () => {
+  const list1 = [2, 0, 2, 1, 1, 0];
+  sortColors(list1);
+  expect(list1).toEqual([0, 0, 1, 1, 2, 2]);
+  const list2 = [2, 0, 1];
+  sortColors(list2);
+  expect(list2).toEqual([0, 1, 2]);
+  const list3 = [0];
+  sortColors(list3);
+  expect(list3).toEqual([0]);
 });
--- src/week-03/index.js
+++ src/week-03/index.js
@@ -33,3 +33,20 @@ export const less = (a, b) => {
 export const swap = (list, i, j, inplace) => {
 
 };
+
+/**
+ * @description 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+ * @sig Array -> Array
+ * @param {Array} nums
+ * @returns {void}
+ * @example
+ *      const colors = [2,0,2,1,1,0]; // 原地排序，之后一样，改变入参
+ *      sortColors(colors);      //=> colors:[0,0,1,1,2,2]
+ *      sortColors([2,0,1]);     //=> [0,1,2]
+ *      sortColors([0]);         //=> [0]
+ *      sortColors([1]);         //=> [1]
+ * @score 60（不使用原生的 sort 方法 20 分，遍历两次数组 20 分，只遍历一次数组 20 分）
+ */
+export const sortColors = (nums) => {
+
+};
